96326bce04b0b9a17dfaf7a50cafaa49ebde6267
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-10-04 09:35:49 -0700

    keep working on Oct 04, 24

--- upload.js
+++ upload.js
@@ -338,3 +338,139 @@ cancel.addEventListener('click', () => {
     uploadTask.cancel();
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import { storage, ref, uploadBytes, uploadBytesResumable, getDownloadURL } from "./firebase.js";
+
+
+let image = document.getElementById('image');
+const upload = document.getElementById('upload');
+
+const start = document.getElementById('start');
+const stop= document.getElementById('stop');
+const cancel = document.getElementById('cancel');
+
+let loader = document.getElementById('loader');
+let state = document.getElementById('state');
+
+let imageUrl;
+
+loader.style.display = 'none';
+
+let uploadTask;
+
+
+const uploadFile = () => {
+    const files = image.files[0];
+
+    console.log(files);
+
+    // validation
+    if(files.size >= 1163561) {
+        console.log('file is too big!');
+        
+    } 
+    else {
+        // with folder
+        // const imagesRefWithFolder = ref(storage, `images/${files.name}`);
+
+        const imagesRefWithFolder = ref(storage, `products/${files.name}`);
+
+        uploadTask = uploadBytesResumable(imagesRefWithFolder, files);
+
+        uploadTask.on('state_changed', 
+            (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+            
+            if(progress === 100) {
+                loader.style.display = 'none';
+                state.innerText = '';
+            }
+            else {
+                state.innerText = 'Upload is ' + progress + '% done';
+                loader.style.display = 'block';
+            }
+
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+                case 'paused':
+                console.log('Upload is paused');
+                break;
+                case 'running':
+                console.log('Upload is running');
+                break;
+            }
+            }, 
+            (error) => {
+            // Handle unsuccessful uploads
+            console.log(error);
+            
+            }, 
+            () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                imageUrl = downloadURL;
+                console.log('File available at', downloadURL);
+            });
+            }
+        ); 
+    }
+}
+
+image.addEventListener('change', uploadFile);
+
+start.addEventListener('click', () => {
+    console.log('start');
+    uploadTask.resume();
+    
+});
+
+stop.addEventListener('click', () => {
+    console.log('stop');
+    uploadTask.pause();
+
+});
+
+cancel.addEventListener('click', () => {
+    console.log('cancel');
+    uploadTask.cancel();
+
+});
+
+
+
+const uploadData = async () => {
+    try {
+        const docRef = await addDoc(collection(db, "users"), {
+          fullName: "Alan",
+          image: imageUrl
+        });
+      
+        console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
+}
+*/
